6a59bc858699302262dfad125115c910e29f78db
TANKING <king@likeyunba.com>
2021-06-10 14:21:37 +0800

    Add files via upload

--- db_config/VersionCheck.php
+++ db_config/VersionCheck.php
@@ -3,10 +3,10 @@
         用于版本更新提示
         获取开发者最新版本
         @Author:TANKING
-        @Web:www.likeyuns.com
-        @CreatDate:2021-05-22
+        @Web:www.likeyunba.com
+        @CreatDate:2021-06-10
     */
-	$version = "6.0.0"; // 当前版本
+	$version = "6.0.1"; // 当前版本
 	$v_file = file_get_contents("http://www.likeyuns.com/api/huoma_new_version.json",true); // 获取最新版本信息
 	$result_arr = json_decode($v_file);
 	$v_str_v = $result_arr->v; // 版本号
