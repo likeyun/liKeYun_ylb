42cbd5229daaa16f950c9bb8d6a749d96eb31c96
TANKING <king@likeyunba.com>
2021-06-11 09:58:25 +0800

    Add files via upload

--- pay/alipay/notify.php
+++ pay/alipay/notify.php
@@ -15,44 +15,49 @@ $tc_days = substr($body,strripos($body,"-")+1);
 // body拆分，获得userid
 $user_id = substr($body,0,strrpos($body,"-"));
 
-// 连接数据库
-$conn = new mysqli($db_url, $db_user, $db_pwd, $db_name);
-mysqli_query($conn,"SET NAMES UTF8");
-// 过滤重复通知
-$sql_check = "SELECT * FROM huoma_order WHERE order_no = '$out_trade_no'";
-$result = $conn->query($sql_check);
-if ($result->num_rows > 0) {
-	echo 'success';
-}else{
-	// 获取当前用户的过期日期
-    $sql_checkuserinfo = "SELECT * FROM huoma_user WHERE user_id = '$user_id'";
-    $result_checkuserinfo = $conn->query($sql_checkuserinfo);
+if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
 
-    if ($result_checkuserinfo->num_rows > 0) {
-
-        while($row_checkuserinfo = $result_checkuserinfo->fetch_assoc()) {
-
-        	// 过期日期
-            $expire_time = $row_checkuserinfo['expire_time'];
-        }
-
-    }else{
-
-        // 获取失败，用当前时间作为默认时间
-        $expire_time = date("Y-m-d");
-    }
-
-    // 插入数据库
-    $sql_insert = "INSERT INTO huoma_order (user_id, order_no, pay_money, xufei_daynum, pay_type) VALUES ('$user_id', '$out_trade_no', '$total_amount', '$tc_days', '支付宝当面付')";
-
-    if ($conn->query($sql_insert) === TRUE) {
-        $daoqi_daynum = $tc_days+1;
-        $new_daoqidate = date('Y-m-d',strtotime("{$expire_time} + ".$daoqi_daynum." day"));
-        $xufei_sql = "UPDATE huoma_user SET expire_time='$new_daoqidate' WHERE user_id=".$user_id;
-        $conn->query($xufei_sql);
+    // 连接数据库
+    $conn = new mysqli($db_url, $db_user, $db_pwd, $db_name);
+    mysqli_query($conn,"SET NAMES UTF8");
+    // 过滤重复通知
+    $sql_check = "SELECT * FROM huoma_order WHERE order_no = '$out_trade_no'";
+    $result = $conn->query($sql_check);
+    if ($result->num_rows > 0) {
         echo 'success';
     }else{
-       $conn->error;
+        // 获取当前用户的过期日期
+        $sql_checkuserinfo = "SELECT * FROM huoma_user WHERE user_id = '$user_id'";
+        $result_checkuserinfo = $conn->query($sql_checkuserinfo);
+
+        if ($result_checkuserinfo->num_rows > 0) {
+
+            while($row_checkuserinfo = $result_checkuserinfo->fetch_assoc()) {
+
+                // 过期日期
+                $expire_time = $row_checkuserinfo['expire_time'];
+            }
+
+        }else{
+
+            // 获取失败，用当前时间作为默认时间
+            $expire_time = date("Y-m-d");
+        }
+
+        // 插入数据库
+        $sql_insert = "INSERT INTO huoma_order (user_id, order_no, pay_money, xufei_daynum, pay_type) VALUES ('$user_id', '$out_trade_no', '$total_amount', '$tc_days', '支付宝当面付')";
+
+        if ($conn->query($sql_insert) === TRUE) {
+            $daoqi_daynum = $tc_days+1;
+            $new_daoqidate = date('Y-m-d',strtotime("{$expire_time} + ".$daoqi_daynum." day"));
+            $xufei_sql = "UPDATE huoma_user SET expire_time='$new_daoqidate' WHERE user_id=".$user_id;
+            $conn->query($xufei_sql);
+            echo 'success';
+        }else{
+           $conn->error;
+        }
     }
+
+    $conn->close();
 }
 ?>
