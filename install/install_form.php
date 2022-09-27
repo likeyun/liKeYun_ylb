e1e8e11591cbd862634acd3116e3cd6cfebc8492
TANKING <king@likeyunba.com>
2022-09-27 14:15:03 +0800

    Update install_form.php

--- install/install_form.php
+++ install/install_form.php
@@ -33,7 +33,7 @@
   <br/>
   <div class="jumbotron" style="background: #f2f2f2;">
     <h2>里客云活码管理系统安装程序</h2>
-    <p>这是一套开源、免费、可上线运营的活码系统，便于协助自己、他人进行微信私域流量资源获取，更大化地进行营销推广活动！降低运营成本，提高工作效率，获取更多资源。<br/><br/><strong>遇到安装问题，可以加入交流群研讨：<a href="javascript:;" data-toggle="modal" data-target="#myModal">点击加群</a></strong></p>
+    <p>这是一套开源、免费、可上线运营的活码系统，便于协助自己、他人进行微信私域流量资源获取，更大化地进行营销推广活动！降低运营成本，提高工作效率，获取更多资源。<br/><br/><strong>遇到安装问题，可以加入交流群研讨：<a href="http://qun.wxpad.cn/ma/common/qun/redirect/?hmid=19122">点击加群</a></strong></p>
 
     <!-- 表单 -->
     <form onsubmit="return false" id="install">
