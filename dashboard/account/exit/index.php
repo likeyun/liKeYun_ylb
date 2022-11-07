07a16cc7a70cc9943a84aaab42553ebd1251629f
TANKING <king@likeyunba.com>
2022-11-07 11:53:09 +0800

    Delete dashboard directory

--- dashboard/account/exit/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-type:text/html;charset=utf-8");
-session_start();
-unset($_SESSION['huoma.dashboard']);
-echo "<script>location.href=\"../../\";</script>";
-?>
--- dashboard/account/loginreg/exit/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-type:text/html;charset=utf-8");
-session_start();
-unset($_SESSION['www.likeyunba.com']);
-echo "<script>location.href=\"../../index.html\";</script>";
-?>
