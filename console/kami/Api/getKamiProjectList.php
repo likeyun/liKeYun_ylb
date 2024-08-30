3220d94cc3c340c264dcf8690ef9be1451f4c2d7
TANKING <king@likeyunba.com>
2024-08-30 17:49:39 +0800

    Add files via upload

--- console/kami/Api/getKamiProjectList.php
+++ console/kami/Api/getKamiProjectList.php
@@ -22,7 +22,7 @@
 	$getKamiProJectList = $db->set_table('ylb_kami')->findAll(
 	    $conditions = ['kami_status' => 1],
 	    $order = null,
-	    $fields = 'kami_title,kami_id,kami_status,kami_repeat_tiqu,kami_repeat_tiqu_interval,kami_type,id',
+	    $fields = 'kami_title,kami_id,kami_status,kami_adStatus,kami_repeat_tiqu,kami_repeat_tiqu_interval,kami_type,id',
 	    $limit = null
 	);
 	
@@ -30,7 +30,7 @@
     $getKamiConfig = $db->set_table('ylb_kamiConfig')->findAll(
 	    $conditions = ['id' => 1],
 	    $order = null,
-	    $fields = 'kmConf_status,kmConf_xcx_title,kmConf_adShow,kmConf_adType,kmConf_bannerID,kmConf_videoID,kmConf_jiliStatus,kmConf_jiliID,kmConf_kfQrcode,kmConf_notification_text',
+	    $fields = 'kmConf_status,kmConf_xcx_title,kmConf_adShow,kmConf_adType,kmConf_jiliStatus,kmConf_btntext,kmConf_kfQrcode,kmConf_notification_text',
 	    $limit = null
 	);
     
@@ -62,31 +62,19 @@
         $kmConf_adShow = false;
     }
     
-    // banner广告ID
-    $kmConf_bannerID = $getKamiConfig[0]['kmConf_bannerID'];
-    
-	// 视频广告ID
-	$kmConf_videoID = $getKamiConfig[0]['kmConf_videoID'];
+    // 提取按钮文字
+    $kmConf_btntext = $getKamiConfig[0]['kmConf_btntext'];
 	
 	// 激励视频广告开启状态
 	if($getKamiConfig[0]['kmConf_jiliStatus'] == 1) {
         
         // 1 激励视频广告开
         $jili_adShow = true;
-        
-        // 激励视频广告开启的时候，提取按钮上的文字
-        // 因为审核要求明确告知用户点击这个按钮是看广告的才给通过
-        // 所以这个文字就用后端返回，过审后你可以改其他的
-	    $tiquButtonText = '看视频免费提取';
     }else {
         
         // 2 激励视频广告关
         $jili_adShow = false;
-        $tiquButtonText = '立即提取';
     }
-    
-    // 激励视频广告ID
-    $kmConf_jiliID = $getKamiConfig[0]['kmConf_jiliID'];
     
 	// 提取页使用的广告类型（1banner 2视频）
 	$kmConf_adType = $getKamiConfig[0]['kmConf_adType'];
@@ -103,12 +91,9 @@
 		    'Service_Status' => $Service_Status, // 服务状态
 		    'Service_Notification_Text' => $Service_Notification_Text, // 公告内容
 		    'kmConf_adShow' => $kmConf_adShow, // 提取页广告开关
-            'kmConf_bannerID' => $kmConf_bannerID, // banner广告ID
-            'kmConf_videoID' => $kmConf_videoID, // 视频广告ID
 		    'jili_adShow' => $jili_adShow, // 激励视频广告开启状态
-		    'kmConf_jiliID' => $kmConf_jiliID, // 激励视频广告ID
 		    'kmConf_adType' => $kmConf_adType, // 提取页使用的广告类型（1banner 2视频）
-		    'tiquButtonText' => $tiquButtonText,
+		    'kmConf_btntext' => $kmConf_btntext, // 提取按钮文字
 		);
 	}else{
 	    
@@ -120,12 +105,9 @@
 		    'Service_Status' => $Service_Status, // 服务状态
 		    'Service_Notification_Text' => $Service_Notification_Text, // 公告内容
 		    'kmConf_adShow' => $kmConf_adShow, // 提取页广告开关
-            'kmConf_bannerID' => $kmConf_bannerID, // banner广告ID
-            'kmConf_videoID' => $kmConf_videoID, // 视频广告ID
 		    'jili_adShow' => $jili_adShow, // 激励视频广告开启状态
-		    'kmConf_jiliID' => $kmConf_jiliID, // 激励视频广告ID
 		    'kmConf_adType' => $kmConf_adType, // 提取页使用的广告类型（1banner 2视频）
-		    'tiquButtonText' => $tiquButtonText,
+		    'kmConf_btntext' => $kmConf_btntext, // 提取按钮文字
         );
 	}
 
