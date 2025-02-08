8cbbd0f079921ce7bfd26aaee431993274de8d33
TANKING <king@likeyunba.com>
2025-02-08 16:29:56 +0800

    Update install.php

--- install/install.php
+++ install/install.php
@@ -684,7 +684,7 @@
                         'db_pass' => $db_pass,
                         'db_prefix' => '',
                         'folderNum' => $install_folder,
-                        'version' => '2.4.3'
+                        'version' => '2.4.4'
                     ];
                     
                     // 生成Db.php文件内容
