c88949859fbc240f3762d242cfab04e8501efd78
TANKING <king@likeyunba.com>
2025-08-07 18:08:47 +0800

    Delete common directory

--- common/dwz/redirect/lx/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    // 获取Key
-    $key = $_GET['key'];
-    
-    // 跳转到落地页
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ../../?key=' . $key . '&t=' . time());
-?>
